751bebf75eae3051fd29c806b4124953407e81c9
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-01-10 20:54:13 +0100

    15. Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary: dict, value):
-    return {k: v for k, v in a_dictionary.items() if v != value}
+    for k in list(a_dictionary.keys()):
+        if a_dictionary[k] == value:
+            del a_dictionary[k]
+    return a_dictionary
