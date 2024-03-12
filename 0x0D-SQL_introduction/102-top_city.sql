cb031580eb6b68bc50c6e7a8106371fd73264c30
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-03-12 20:56:31 +0000

    top 3

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- top 3
-SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month IN ("JULY", "August ") GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month BETWEEN '7' AND '8' GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
