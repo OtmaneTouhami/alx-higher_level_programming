9e409f776de166089f0db43ad81c200c7babd8ea
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-04-25 14:51:19 +0100

    0x10. Python - Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of the response of a curl POST request
-curl -s 0.0.0.0:5000/catch_me?cmd=$(printf "printf\047You got me!\047")
+curl -sLX PUT -d "user_id=98" -H "Origin:HolbertonSchool" 0.0.0.0:5000/catch_me
